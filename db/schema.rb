9d9287d607b27edee8408a9a5ff88ec051b8a987
papua <nakao_shinichirou@aratana.jp>
2014-10-04 23:38:31 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004140953) do
+ActiveRecord::Schema.define(version: 20141004143342) do
+
+  create_table "dashboards", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "provided_books", force: true do |t|
     t.string   "name"
