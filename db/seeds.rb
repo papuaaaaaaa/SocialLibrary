22f6e5d4df9215d0f9b40fda32f3cc45eb96e2c4
papua <nakao_shinichirou@aratana.jp>
2014-10-16 20:43:37 +0900

    temporary seed for Community

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Community.create(:name => 'aratana', :url => 'http://www.aratana.jp');
